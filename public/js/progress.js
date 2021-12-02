de8c1cd5cf85efa47608ea803956d5f335f8a445
Vasco <vascoeti123@gmail.com>
2021-12-02 14:30:48 +0200

    update

--- public/js/progress.js
+++ public/js/progress.js
@@ -1,4 +1,4 @@
-var updateProgressBar(progressBar, value){
+var updateProgressBar = function (progressBar, value) {
     value = Math.round(value);
     progressBar.querySelector(".progress__fill").style.width = `${value}`;
     progressBar.querySelector(".progress__text").textContent = `${value}`;
